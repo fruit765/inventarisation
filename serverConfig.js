cb36b6607c632f5e4abb446406c12f0ae4ef7edd
fruit765 <megafpykt@ya.ru>
2020-10-23 17:13:42 +0300

    init

---
+++ serverConfig.js
@@ -0,0 +1,26 @@
+module.exports = {
+    server: {
+        port: "3000"
+    },
+
+    db: {
+        client: "mysql2",
+        connection: {
+            host: "127.0.0.1",
+            user: "root",
+            password: "",
+            database: "inv",
+            timezone: '+00:00'
+        }
+    },
+
+    cors: {
+        credentials: true,
+        origin: true
+    },
+
+    session: {
+        maxAge: 15000,
+        secret: "C!dH}4bA7+w=![m.B;MS(bLf!LU<<p)MY;%qQXW7sE]y"
+    }
+}
---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+'use strict'
+
+const express = require("express")
+const app = express()
+const port = require("../serverConfig").server.port
+
+require("./middlewares")(app)
+require("./routes.js")(app)
+
+
+app.listen(port)
